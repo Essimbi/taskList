187869511fe98ea1777762c97308e534a8dffe86
Essimbi <essimbideranot@gmail.com>
2024-02-09 11:46:42 +0100

    Ajout du menu des ajouts

--- src/screen/home/index.js
+++ src/screen/home/index.js
@@ -1,8 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React from "react";
 import style from "./style";
-import { AddIcon, Input, ThreeDotsIcon, Icon } from "native-base";
-import { MaterialIcons } from '@expo/vector-icons';
+import { AddIcon, Box, HamburgerIcon, Input, Menu } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Home = () => {
   return (
@@ -11,13 +11,28 @@ const Home = () => {
       <View style={style.hearder}>
         {/* <Icon name="menu" size="30" color="white" /> */}
         <MaterialIcons name="menu" size={30} color="black" />
-        <Input mx="3" placeholder="Search task here..." w="80%" rounded={50}/>
-        <AddIcon name="menu" size="5" color="black" />
+        <Input mx="3" placeholder="Search task here..." w="80%" rounded={50} />
+
+        <Menu
+          w="190"
+          trigger={(triggerProps) => {
+            return (
+              <Pressable
+                accessibilityLabel="More options menu"
+                {...triggerProps}
+              >
+                <AddIcon name="menu" size="5" color="black" />
+              </Pressable>
+            );
+          }}
+        >
+          <Menu.Item>New category</Menu.Item>
+          <Menu.Item>New task</Menu.Item>
+        </Menu>
       </View>
       {/* Fin du Header */}
     </View>
   );
-
 };
 
 export default Home;
